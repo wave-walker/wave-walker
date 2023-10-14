820c21f1fc248f230d64219211bb9a16b855ce93
Armin Kirchner <post.armin@gmail.com>
2023-10-14 16:49:47 +0900

    Automatically create partition for asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,11 @@
 class Asset < ApplicationRecord
   has_many :trades, dependent: :restrict_with_error
+
+  after_create :add_trade_partition_for_asset
+
+  private
+
+  def add_trade_partition_for_asset
+    Trade.create_partition_for_asset(id, name)
+  end
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,3 +1,12 @@
 class Trade < ApplicationRecord
   belongs_to :asset
+
+  def self.create_partition_for_asset(asset_id, asset_name)
+    sql = <<-SQL
+      CREATE TABLE asset_#{asset_name.downcase}_trades PARTITION OF trades
+        FOR VALUES IN (#{asset_id});
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
