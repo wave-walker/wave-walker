4f2eb723eb322e098559225b091de115b24680eb
Armin Kirchner <post.armin@gmail.com>
2023-10-14 16:40:26 +0900

    Partition trades by asset_id

---
+++ db/migrate/20231014070704_partition_trades.rb
@@ -0,0 +1,38 @@
+class PartitionTrades < ActiveRecord::Migration[7.1]
+  def up
+    drop_table :trades
+
+    execute <<-SQL
+      CREATE SEQUENCE trades_id_seq
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1
+    SQL
+
+    create_table(:trades, primary_key: [:asset_id, :id], options: "PARTITION BY LIST (asset_id)") do |t|
+      t.bigint :id, null: false
+      t.belongs_to :asset, null: false, foreign_key: true, index: false
+      t.decimal :price, precision: 20, scale: 10, null: false
+      t.decimal :volume, precision: 20, scale: 10, null: false
+
+      t.timestamp :created_at, null: false
+    end
+
+    execute "ALTER SEQUENCE trades_id_seq OWNED BY trades.id"
+    execute "ALTER TABLE ONLY trades ALTER COLUMN id SET DEFAULT nextval('trades_id_seq'::regclass)"
+  end
+
+  def down
+    drop_table :trades
+
+    create_table :trades do |t|
+      t.decimal :price, precision: 20, scale: 10, null: false
+      t.decimal :volume, precision: 20, scale: 10, null: false
+
+      t.timestamp :created_at, null: false
+      t.references :asset, null: false, foreign_key: true
+    end
+  end
+end
---
+++ db/structure.sql
@@ -0,0 +1,172 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: assets; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.assets (
+    id bigint NOT NULL,
+    name character varying NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: assets_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.assets_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: assets_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.assets_id_seq OWNED BY public.assets.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+--
+-- Name: trades; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.trades (
+    id bigint NOT NULL,
+    asset_id bigint NOT NULL,
+    price numeric(20,10) NOT NULL,
+    volume numeric(20,10) NOT NULL,
+    created_at timestamp without time zone NOT NULL
+)
+PARTITION BY LIST (asset_id);
+
+
+--
+-- Name: trades_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.trades_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: trades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.trades_id_seq OWNED BY public.trades.id;
+
+
+--
+-- Name: assets id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.assets ALTER COLUMN id SET DEFAULT nextval('public.assets_id_seq'::regclass);
+
+
+--
+-- Name: trades id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.trades ALTER COLUMN id SET DEFAULT nextval('public.trades_id_seq'::regclass);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: assets assets_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.assets
+    ADD CONSTRAINT assets_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: trades trades_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.trades
+    ADD CONSTRAINT trades_pkey PRIMARY KEY (asset_id, id);
+
+
+--
+-- Name: index_assets_on_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_assets_on_name ON public.assets USING btree (name);
+
+
+--
+-- Name: trades fk_rails_25d084c853; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE public.trades
+    ADD CONSTRAINT fk_rails_25d084c853 FOREIGN KEY (asset_id) REFERENCES public.assets(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+SET search_path TO "$user", public;
+
+INSERT INTO "schema_migrations" (version) VALUES
+('20231014070704'),
+('20231014055916'),
+('20231011165601');
+
