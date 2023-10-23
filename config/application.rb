8c5ca3b3747d9a4c3832db3d7d2ca61d1c56e86c
Armin Kirchner <post.armin@gmail.com>
2023-10-24 01:26:41 +0900

    Setup que for asynchronous job processing

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module WaveWalker
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.schema_format = :sql
+    config.active_job.queue_adapter = :que
   end
 end
