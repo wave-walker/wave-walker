fec82087a6de7272d82518e84a21f364b0702898
Armin Kirchner <post.armin@gmail.com>
2023-10-24 01:08:05 +0900

    Ensure failed Kraken request raise an error

---
+++ test/lib/kraken_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class KrakenTest < ActiveSupport::TestCase
+  class TradesTest < ActiveSupport::TestCase
+    test "too many request raise an error" do
+      stub_request(:get, "https://api.kraken.com/0/public/Trades?count=10000&pair=XXBTZEUR&since=0").
+        to_return(
+          status: 200,
+          body: { error: ["EGeneral:Too many requests"] }.to_json,
+          headers: { 'Content-Type' => 'application/json' })
+
+      error = assert_raises Kraken::RateLimitExceeded do
+        Kraken.trades(pair: 'XXBTZEUR', since: 0)
+      end
+
+      assert_equal "EGeneral:Too many requests", error.message
+    end
+
+    test "unkown errors raise an error" do
+      stub_request(:get, "https://api.kraken.com/0/public/Trades?count=10000&pair=XXBTZEUR&since=0").
+        to_return(
+          status: 200,
+          body: { error: ["Some unexpected happend!"] }.to_json,
+          headers: { 'Content-Type' => 'application/json' })
+
+      error = assert_raises Kraken::Error do
+        Kraken.trades(pair: 'XXBTZEUR', since: 0)
+      end
+
+      assert_equal "Some unexpected happend!", error.message
+    end
+
+    test "returns the trades and the cursor position" do
+      stub_request(:get, "https://api.kraken.com/0/public/Trades?count=10000&pair=XXBTZEUR&since=0").
+        to_return(
+          status: 200,
+          body: { error: [], result: { 'XXBTZEUR' => [[1, 2, 3, 4, 5, 6]], last: 123456 } }.to_json,
+          headers: { 'Content-Type' => 'application/json' })
+
+      response = Kraken.trades(pair: 'XXBTZEUR', since: 0)
+
+      assert_equal [[1, 2, 3, 4, 5, 6]], response[:trades]
+      assert_equal 123456, response[:last]
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/mock"
+require "webmock/minitest"
 
 module ActiveSupport
   class TestCase
