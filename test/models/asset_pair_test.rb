f1925223f0aeb9886462f57f9b8ba6360b206f47
Armin Kirchner <post.armin@gmail.com>
2023-11-21 23:49:20 +0900

    Test that trade sync is enqueued once at a time

--- test/models/asset_pair_test.rb
+++ test/models/asset_pair_test.rb
@@ -1,17 +1,14 @@
 require "test_helper"
 
 class AssetPairTest < ActiveSupport::TestCase
-  test "#import_later, enqueues a job to import trades" do
-    perform_later_mock = Minitest::Mock.new
-    perform_later_mock.expect :call, nil, [asset_pairs(:atomusd)]
+  include ActiveJob::TestHelper
 
-    TradeSyncJob.stub(:perform_later, perform_later_mock) do
+  test "#import_later, enqueues a job to import trades" do
+    assert_enqueued_with(job: TradeSyncJob, args: [asset_pairs(:atomusd)]) do
       assert_changes -> { asset_pairs(:atomusd).reload.import_state }, to: 'importing' do
         asset_pairs(:atomusd).import_later
       end
     end
-
-    assert_mock perform_later_mock
   end
 
   test "#import_later, raises an error if already importing" do
@@ -27,8 +24,10 @@ class AssetPairTest < ActiveSupport::TestCase
   test "#import_later, change to waiting when another token is importing" do
     asset_pairs(:btcusd).importing!
 
-    assert_changes -> { asset_pairs(:atomusd).reload.import_state }, to: 'waiting' do
-      asset_pairs(:atomusd).import_later
+    assert_no_enqueued_jobs(only: TradeSyncJob) do
+      assert_changes -> { asset_pairs(:atomusd).reload.import_state }, to: 'waiting' do
+        asset_pairs(:atomusd).import_later
+      end
     end
   end
 
@@ -36,15 +35,10 @@ class AssetPairTest < ActiveSupport::TestCase
     asset_pairs(:atomusd).waiting!
     asset_pairs(:btcusd).waiting!
 
-    perform_later_mock = Minitest::Mock.new
-    perform_later_mock.expect :call, nil, [asset_pairs(:btcusd)]
-
-    TradeSyncJob.stub(:perform_later, perform_later_mock) do
+    assert_enqueued_with(job: TradeSyncJob, args: [asset_pairs(:btcusd)]) do
       assert_changes -> { asset_pairs(:btcusd).reload.import_state }, to: 'importing' do
         AssetPair.import_waiting_later
       end
     end
-
-    assert_mock perform_later_mock
   end
 end
