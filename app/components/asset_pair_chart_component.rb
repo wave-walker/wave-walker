cda7169f01769a0ec5d8c9cc6e906551019fadd2
Armin Kirchner <post.armin@gmail.com>
2024-02-27 09:57:23 +0900

    Visually test cart data

--- app/components/asset_pair_chart_component.rb
+++ app/components/asset_pair_chart_component.rb
@@ -7,6 +7,10 @@ class AssetPairChartComponent < ViewComponent::Base
     super
   end
 
+  def id
+    "asset_pair_chard_#{asset_pair.id}"
+  end
+
   def tile
     "#{asset_pair.name} #{timeframe}"
   end
@@ -14,7 +18,7 @@ class AssetPairChartComponent < ViewComponent::Base
   def candlestick_series
     ohlcs.map do |ohlc|
       {
-        time: ohlc.range.end,
+        time: ohlc.range.end.to_i,
         open: ohlc.open,
         high: ohlc.high,
         low: ohlc.low,
@@ -26,7 +30,7 @@ class AssetPairChartComponent < ViewComponent::Base
   def volume_series
     ohlcs.map do |ohlc|
       color = ohlc.open < ohlc.close ? 'green' : 'red'
-      { time: ohlc.range.end, value: ohlc.volume, color: }
+      { time: ohlc.range.end.to_i, value: ohlc.volume, color: }
     end
   end
 
