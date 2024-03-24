9936405499171e5abff5525e9ab4e990ede585c4
Armin Kirchner <post.armin@gmail.com>
2024-03-25 01:02:29 +0900

    Simplify and harden Kraken trade imports
    The public Kraken API is rate limited to 1 request per IP and second.
    Limiting the requests allow us to stay under the rate limit. Enumerating
    the API responses allow for am easily testable and isolated import logic.

---
+++ app/enumerators/kraken_trades_enumerator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class KrakenTradesEnumerator
+  extend Limiter::Mixin
+
+  def self.call(asset_pair, cursor:) = new(asset_pair, cursor:).to_enum(:each).lazy
+
+  # Kraken allows 1 request per second.
+  limit_method :load_trades, rate: 60, balanced: true
+
+  def initialize(asset_pair, cursor:)
+    @asset_pair = asset_pair
+    @cursor = cursor.to_i
+  end
+
+  def each
+    loop do
+      trades = load_trades
+      break if trades.empty?
+
+      yield trades, cursor
+    end
+  end
+
+  private
+
+  attr_reader :asset_pair
+  attr_accessor :cursor
+
+  def load_trades
+    response = Kraken.trades(pair: asset_pair.name, since: cursor)
+    self.cursor = response.fetch(:last)
+
+    response.fetch(:trades)
+  end
+end
---
+++ test/enumerators/kraken_trades_enumerator_test.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class KrakenTradesEnumeratorTest < ActiveSupport::TestCase
+  test 'request the first trades if latest trade is not present' do
+    asset_pair = AssetPair.new(name: 'ATOMUSD')
+    Kraken.expects(:trades).with(pair: 'ATOMUSD', since: 0)
+          .returns({ trades: [], last: 1 })
+
+    KrakenTradesEnumerator.call(asset_pair, cursor: nil).first
+  end
+
+  test 'continues import after the latest trade' do
+    asset_pair = AssetPair.new(name: 'ATOMUSD')
+    Kraken.expects(:trades).with(pair: 'ATOMUSD', since: 1234)
+          .returns({ trades: [], last: 1 })
+
+    KrakenTradesEnumerator.call(asset_pair, cursor: Time.zone.at(1234)).first
+  end
+
+  test 'load next trades with the cursor position and ends with no trades' do
+    Limiter::Clock.stubs(:sleep)
+
+    asset_pair = AssetPair.new(name: 'ATOMUSD')
+    Kraken.expects(:trades).with(pair: 'ATOMUSD', since: 0)
+          .returns({ trades: %i[trade_a trade_b], last: 3 })
+
+    Kraken.expects(:trades).with(pair: 'ATOMUSD', since: 3)
+          .returns({ trades: [], last: 4 })
+
+    KrakenTradesEnumerator.call(asset_pair, cursor: nil).to_a
+  end
+
+  test 'yields the trades and cursor' do
+    asset_pair = AssetPair.new(name: 'ATOMUSD')
+
+    trades = %i[trade_a trade_b]
+    cursor = 2
+
+    Kraken.expects(:trades).with(pair: 'ATOMUSD', since: 0)
+          .returns({ trades:, last: cursor })
+
+    trades_yield = KrakenTradesEnumerator.call(asset_pair, cursor: nil).first
+    assert_equal trades_yield[0], trades
+    assert_equal trades_yield[1], cursor
+  end
+
+  setup do
+    KrakenTradesEnumerator.reset_load_trades_limit!
+  end
+
+  test 'rate limites the API requests to 1 per second' do
+    Limiter::Clock.expects(:sleep).with do |sleep_time|
+      assert_in_delta sleep_time, 1, 0.1
+    end
+
+    asset_pair = AssetPair.new(name: 'ATOMUSD')
+    Kraken.stubs(:trades).with(pair: 'ATOMUSD', since: 0)
+          .returns({ trades: %i[trade_a trade_b], last: 3 })
+
+    Kraken.stubs(:trades).with(pair: 'ATOMUSD', since: 3)
+          .returns({ trades: [], last: 4 })
+
+    KrakenTradesEnumerator.call(asset_pair, cursor: nil).to_a
+  end
+end
