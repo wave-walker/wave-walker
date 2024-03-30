3f6f56140ec09728329c71e4496c28f2f266a548
Armin Kirchner <post.armin@gmail.com>
2024-03-30 10:17:27 +0900

    Chore: Migrate remaining test stubs to Mocha

--- test/jobs/trigger_ohlc_generation_job_test.rb
+++ test/jobs/trigger_ohlc_generation_job_test.rb
@@ -7,12 +7,23 @@ class TriggerOhlcGenerationJobTest < ActiveJob::TestCase
     timeframes = Ohlc.timeframes.keys
     atomusd = asset_pairs(:atomusd)
     btcusd = asset_pairs(:btcusd)
+    atomusd.update!(imported_until: Time.current)
+    btcusd.update!(imported_until: Time.current)
 
     TriggerOhlcGenerationJob.perform_now
 
     timeframes.each do |timeframe|
       assert_enqueued_with(job: OhlcJob, args: [{ asset_pair: atomusd, timeframe: }])
       assert_enqueued_with(job: OhlcJob, args: [{ asset_pair: btcusd, timeframe: }])
+    end
+  end
+
+  test 'dose not enuqueue OHLC creation for assets have no imports' do
+    asset_pairs(:atomusd)
+    asset_pairs(:btcusd)
+
+    assert_no_enqueued_jobs do
+      TriggerOhlcGenerationJob.perform_now
     end
   end
 end
