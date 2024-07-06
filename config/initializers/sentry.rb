0344c33f1ad3a334251042211034d0da365f436c
Armin Kirchner <post.armin@gmail.com>
2024-07-06 16:18:37 +0900

    Send good_job exceptions to Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+GoodJob.on_thread_error = ->(exception) { Sentry.capture_exception(exception) }
+
 Sentry.init do |config|
   config.dsn = ENV.fetch('SENTRY_DNS', nil)
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
