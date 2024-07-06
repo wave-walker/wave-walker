ab30b82c9541926b24959233d3d09b477f7586f3
Armin Kirchner <post.armin@gmail.com>
2024-07-06 16:06:03 +0900

    fix: rubocop pathes

--- db/migrate/20240504162349_no_null_for_asset_pair_parameter.rb
+++ db/migrate/20240504162349_no_null_for_asset_pair_parameter.rb
@@ -2,7 +2,7 @@
 
 class NoNullForAssetPairParameter < ActiveRecord::Migration[7.1]
   def change
-    change_column_null :asset_pairs, :base, false
+    change_column_null :asset_pairs, :base, false # rubocop:disable Rails/BulkChangeTable
     change_column_null :asset_pairs, :quote, false
     change_column_null :asset_pairs, :cost_decimals, false
   end
