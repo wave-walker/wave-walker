cb72e6872d7acc86e367e7d79a11f226b6067938
Armin Kirchner <post.armin@gmail.com>
2024-07-06 16:39:55 +0900

    Don't preserve good_job jobs
    The server of the application is small and many jobs are schedualed.
    Removing processed jobs allows for a smother job opperation.

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  config.good_job.preserve_job_records = true
   config.good_job.enable_cron = true
   config.good_job.queues = '+critical,default,low'
 
