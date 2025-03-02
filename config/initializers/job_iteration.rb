f41de075f844aa243e27bfed2b31e33bd4998d21
Armin Kirchner <post.armin@gmail.com>
2025-03-02 22:08:52 +0900

    Use default iteration job adapter
    Iteration job now ships with an adapter that supports
    the good_job worker.

--- config/initializers/job_iteration.rb
+++ config/initializers/job_iteration.rb
@@ -1,17 +1,3 @@
 # frozen_string_literal: true
 
 JobIteration.max_job_runtime = 5.minutes
-
-module JobIteration
-  module InterruptionAdapters
-    module GoodJobAdapter
-      class << self
-        def call
-          !!::GoodJob.current_thread_shutting_down?
-        end
-      end
-    end
-
-    register(:good_job, GoodJobAdapter)
-  end
-end
