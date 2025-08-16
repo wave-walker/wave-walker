b4584a2bf396b881b04e40df5fbaa45b0a257074
Armin Kirchner <post.armin@gmail.com>
2025-08-16 23:09:03 +0200

    CI fix

--- db/migrate/20231021145729_add_kraken_cursor_position_to_assets.rb
+++ db/migrate/20231021145729_add_kraken_cursor_position_to_assets.rb
@@ -2,7 +2,7 @@
 
 class AddKrakenCursorPositionToAssets < ActiveRecord::Migration[7.1]
   def change
-    remove_column :assets, :last_synced_trade_at, :timestamp # rubocop:todo Rails/BulkChangeTable
+    remove_column :assets, :last_synced_trade_at, :timestamp
     add_column :assets, :kraken_cursor_position, :bigint, default: 0, null: false
   end
 end
--- db/migrate/20240417144243_add_flip_to_smoothed_trends.rb
+++ db/migrate/20240417144243_add_flip_to_smoothed_trends.rb
@@ -2,7 +2,7 @@
 
 class AddFlipToSmoothedTrends < ActiveRecord::Migration[7.1]
   def change
-    add_column :smoothed_trends, :flip, :boolean, null: false, default: false # rubocop:disable Rails/BulkChangeTable
+    add_column :smoothed_trends, :flip, :boolean, null: false, default: false
     change_column_default :smoothed_trends, :flip, from: false, to: nil
   end
 end
