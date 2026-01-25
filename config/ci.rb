421999ccdd765c43ca05fd6776611a244bc87647
Armin Kirchner <post.armin@gmail.com>
2026-01-25 15:25:22 +0100

    Bumped Rails to 8.1 with dependency update

---
+++ config/ci.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Run using bin/ci
+
+CI.run do
+  step 'Setup', 'bin/setup --skip-server'
+
+  step 'Style: Ruby', 'bin/rubocop'
+
+  step 'Security: Importmap vulnerability audit', 'bin/importmap audit'
+  step 'Security: Brakeman code analysis', 'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
+  step 'Tests: Rails', 'bin/rails test'
+  step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
+
+  step 'Tests: System', 'bin/rails test:system'
+end
